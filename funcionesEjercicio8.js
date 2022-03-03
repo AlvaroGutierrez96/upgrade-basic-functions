dc7fe3fde5260cec2654d11c518593a8d3f4de94
AlvaroGutierrez96 <alvaro.gutierrez@bootcamp-upgrade.com>
2022-03-03 22:51:21 +0100

    Iteration#8#

--- funcionesEjercicio8.js
+++ funcionesEjercicio8.js
@@ -1,19 +1,33 @@
-// un div dentro del body que tenga dentro de el un h1
+/* Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma. 
+Puedes usar este array para probar tu función: */
 
-/* let newP = document.createElement('p'); */
+let counterWords = [
+    'code',
+    'repeat',
+    'eat',
+    'sleep',
+    'code',
+    'enjoy',
+    'sleep',
+    'code',
+    'enjoy',
+    'upgrade',
+    'code'
+  ];
+    function repeatCounter(array) {
+    let wordCounter = 0;
+    let counteredWords = [];
+    for (let i = 0; i < array.length; i++) {
+        wordCounter=0;
+        if(!counteredWords.includes(array[i])){
+            for (let j = 0; j < array.length; j++) {
+                if (array[i] === array[j]) {
+                    wordCounter++;
+                    counteredWords.push(array[i]);
+                }   
+            } console.log('La palabra '+ array[i]+' se repite '+wordCounter+' veces.');       
+        } 
+    } 
+    }
 
-let newD = document.createElement ('div')
-
-let newH1 = document.createElement ('h1')
-
-/* newP.textContent = 'Hola'; */
-
-newH1.textContent = 'Este es mi H1'
-
-
-
-/* document.body.appendChild(newP) */
-
-newD.appendChild(newH1)
-
-document.body.appendChild(newD)
+  repeatCounter(counterWords);
